25e268cc37f8d2dee880339134316bfefd465b23
phanthituthi <83085963+phanthituthi@users.noreply.github.com>
2021-09-26 14:02:24 +0700

    Initial commit

---
+++ src/main/java/com/iuh/SpringBoot_JWT_pttt/SpringBootJwtPtttApplication.java
@@ -0,0 +1,13 @@
+package com.iuh.SpringBoot_JWT_pttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtPtttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtPtttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iuh/SpringBoot_JWT_pttt/SpringBootJwtPtttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.SpringBoot_JWT_pttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtPtttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
